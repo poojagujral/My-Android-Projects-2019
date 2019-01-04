9b35528f2472b9aa72390448bb617186a008edd9
Pooja Gujral <40609641+poojagujral@users.noreply.github.com>
2019-01-05 01:22:30 +0530

    Android app created to show the use of Intents for jumping from one activity to another.

---
+++ app/src/main/java/com/example/poojagujral/helloworld/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.poojagujral.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain2activity();
+            }
+        });
+    }
+    //open second activity
+    public void openMain2activity(){
+        Intent intent= new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
